bb1d2c7d1c2de86ae15705df56a12a5512cf8aa9
reshmabidikar <reshmabidikar@outlook.com>
2025-06-24 09:39:04 +0530

    create/change addon with recurring+usage override

--- src/main/java/org/killbill/billing/plugin/helloworld/HelloWorldListener.java
+++ src/main/java/org/killbill/billing/plugin/helloworld/HelloWorldListener.java
@@ -30,6 +30,14 @@ import org.killbill.billing.account.api.AccountApiException;
 import org.killbill.billing.catalog.api.Currency;
 import org.killbill.billing.catalog.api.PlanPhasePriceOverride;
 import org.killbill.billing.catalog.api.PlanPhaseSpecifier;
+import org.killbill.billing.catalog.api.TierPriceOverride;
+import org.killbill.billing.catalog.api.TieredBlockPriceOverride;
+import org.killbill.billing.catalog.api.UsagePriceOverride;
+import org.killbill.billing.catalog.api.UsageType;
+import org.killbill.billing.catalog.api.boilerplate.PlanPhasePriceOverrideImp;
+import org.killbill.billing.catalog.api.boilerplate.TierPriceOverrideImp;
+import org.killbill.billing.catalog.api.boilerplate.TieredBlockPriceOverrideImp;
+import org.killbill.billing.catalog.api.boilerplate.UsagePriceOverrideImp;
 import org.killbill.billing.entitlement.api.BaseEntitlementWithAddOnsSpecifier;
 import org.killbill.billing.entitlement.api.Entitlement;
 import org.killbill.billing.entitlement.api.EntitlementApiException;
@@ -108,9 +116,12 @@ public class HelloWorldListener implements OSGIKillbillEventDispatcher.OSGIKillb
         //base
         final EntitlementSpecifier baseSpec = new PluginEntitlementSpecifier.Builder().withPlanPhaseSpecifier(new PlanPhaseSpecifier("standard-monthly-in-arrear")).build();
 
-        //addon with recuring price override 1.60
-        PlanPhasePriceOverride addonOverride = new PluginPlanPhasePriceOverride.Builder().withPhaseName("ao1-in-arrear-evergreen").withRecurringPrice(new BigDecimal("1.60")).withCurrency(Currency.USD).build();
-        final EntitlementSpecifier addonSpec = new PluginEntitlementSpecifier.Builder().withPlanPhaseSpecifier(new PlanPhaseSpecifier("ao1-in-arrear")).withOverrides(List.of(addonOverride)).build();
+        //addon with recurring price override of 1.60 and usage price override of 1.25
+        TieredBlockPriceOverride tieredBlockPriceOverride = new TieredBlockPriceOverrideImp.Builder<>().withUnitName("bullets").withSize(new BigDecimal(1)).withPrice(new BigDecimal(1.25)).withCurrency(Currency.USD).withMax(new BigDecimal(1000)).build();
+        TierPriceOverride tierPriceOverride = new TierPriceOverrideImp.Builder<>().withTieredBlockPriceOverrides(List.of(tieredBlockPriceOverride)).build();
+        UsagePriceOverride usagePriceOverride = new UsagePriceOverrideImp.Builder<>().withName("ao3-usage").withUsageType(UsageType.CONSUMABLE).withTierPriceOverrides(List.of(tierPriceOverride)).build();
+        PlanPhasePriceOverride addonOverride = new PlanPhasePriceOverrideImp.Builder<>().withPhaseName("ao3-in-arrear-evergreen").withRecurringPrice(new BigDecimal("1.60")).withUsagePriceOverrides(List.of(usagePriceOverride)).withCurrency(Currency.USD).build();
+        final EntitlementSpecifier addonSpec = new PluginEntitlementSpecifier.Builder().withPlanPhaseSpecifier(new PlanPhaseSpecifier("ao3-in-arrear")).withOverrides(List.of(addonOverride)).build();
 
         //bundle
         final BaseEntitlementWithAddOnsSpecifier cartSpecifier = new BaseEntitlementWithAddOnsSpecifierImp.Builder<>().withEntitlementSpecifier(List.of(baseSpec, addonSpec)).build();
@@ -120,48 +131,111 @@ public class HelloWorldListener implements OSGIKillbillEventDispatcher.OSGIKillb
         final List<UUID> allEntitlements = osgiKillbillAPI.getEntitlementApi().createBaseEntitlementsWithAddOns(account.getId(), List.of(cartSpecifier), true, Collections.emptyList(), callContext);
 
         //Comment the code below and  Retrieve addon subscription via https://killbill.github.io/slate/subscription.html#retrieve-a-subscription-by-id
-        // verify that the prices section lists the overridden price
+        // verify that the prices section lists the overridden price: 1.6 recurring and 1.25 usage
 //        "prices": [
 //        {
-//            "planName": "ao1-in-arrear-32",
-//                "phaseName": "ao1-in-arrear-32-evergreen",
+//            "planName": "ao3-in-arrear-51",
+//                "phaseName": "ao3-in-arrear-51-evergreen",
 //                "phaseType": "EVERGREEN",
 //                "fixedPrice": null,
 //                "recurringPrice": 1.6,
-//                "usagePrices": []
+//                "usagePrices": [
+//            {
+//                "usageName": "ao3-usage",
+//                    "usageType": "CONSUMABLE",
+//                    "billingMode": "IN_ARREAR",
+//                    "tierBlockPolicy": "ALL_TIERS",
+//                    "tierPrices": [
+//                {
+//                    "blockPrices": [
+//                    {
+//                        "unitName": "bullets",
+//                            "size": 1,
+//                            "price": 1.25,
+//                            "max": 1000
+//                    }
+//              ]
+//                }
+//          ]
+//            }
+//      ]
 //        }
-//  ]
+//  ],
+
 
         //schedule plan change for addon one month from now
         final UUID addonEntId = allEntitlements.get(1);
         final Entitlement addonEnt = osgiKillbillAPI.getEntitlementApi().getEntitlementForId(addonEntId, false, callContext);
-        addonOverride = new PluginPlanPhasePriceOverride.Builder().withPhaseName("ao1-in-arrear-evergreen").withRecurringPrice(new BigDecimal("1.75")).withCurrency(Currency.USD).build();
-        final EntitlementSpecifier addonSpec2 = new PluginEntitlementSpecifier.Builder().withPlanPhaseSpecifier(new PlanPhaseSpecifier("ao1-in-arrear")).withOverrides(List.of(addonOverride)).build();
+
+        //plan change with recurring price override of 1.75 and usage price override of 1.35
+        tieredBlockPriceOverride = new TieredBlockPriceOverrideImp.Builder<>().withUnitName("bullets").withSize(new BigDecimal(1)).withPrice(new BigDecimal(1.35)).withCurrency(Currency.USD).withMax(new BigDecimal(1000)).build();
+        tierPriceOverride = new TierPriceOverrideImp.Builder<>().withTieredBlockPriceOverrides(List.of(tieredBlockPriceOverride)).build();
+        usagePriceOverride = new UsagePriceOverrideImp.Builder<>().withName("ao3-usage").withUsageType(UsageType.CONSUMABLE).withTierPriceOverrides(List.of(tierPriceOverride)).build();
+        addonOverride = new PlanPhasePriceOverrideImp.Builder<>().withPhaseName("ao3-in-arrear-evergreen").withRecurringPrice(new BigDecimal("1.75")).withUsagePriceOverrides(List.of(usagePriceOverride)).withCurrency(Currency.USD).build();
+
+        final EntitlementSpecifier addonSpec2 = new PluginEntitlementSpecifier.Builder().withPlanPhaseSpecifier(new PlanPhaseSpecifier("ao3-in-arrear")).withOverrides(List.of(addonOverride)).build();
         addonEnt.changePlanWithDate(addonSpec2, clock.getClock().getUTCToday().plusMonths(1), Collections.emptyList(), callContext);
 
         //Retrieve addon subscription via https://killbill.github.io/slate/subscription.html#retrieve-a-subscription-by-id and
         // verify that the prices section lists the overridden price
-        //This show 2 recurring prices: 1.60 and 1.75:
+        //This show 2 recurring prices: 1.60 and 1.75 and 2 usage prices 1.25 and 1.35:
 
-        //                    "prices": [
-        //                    {
-        //                        "planName": "ao1-in-arrear-36",
-        //                            "phaseName": "ao1-in-arrear-36-evergreen",
-        //                            "phaseType": "EVERGREEN",
-        //                            "fixedPrice": null,
-        //                            "recurringPrice": 1.6,
-        //                            "usagePrices": []
-        //                    },
-        //                    {
-        //                        "planName": "ao1-in-arrear-37",
-        //                            "phaseName": "ao1-in-arrear-37-evergreen",
-        //                            "phaseType": "EVERGREEN",
-        //                            "fixedPrice": null,
-        //                            "recurringPrice": 1.75,
-        //                            "usagePrices": []
-        //                    }
-        //  ]
-
+//        "prices": [
+//        {
+//            "planName": "ao3-in-arrear-51",
+//                "phaseName": "ao3-in-arrear-51-evergreen",
+//                "phaseType": "EVERGREEN",
+//                "fixedPrice": null,
+//                "recurringPrice": 1.6,
+//                "usagePrices": [
+//            {
+//                "usageName": "ao3-usage",
+//                    "usageType": "CONSUMABLE",
+//                    "billingMode": "IN_ARREAR",
+//                    "tierBlockPolicy": "ALL_TIERS",
+//                    "tierPrices": [
+//                {
+//                    "blockPrices": [
+//                    {
+//                        "unitName": "bullets",
+//                            "size": 1,
+//                            "price": 1.25,
+//                            "max": 1000
+//                    }
+//              ]
+//                }
+//          ]
+//            }
+//      ]
+//        },
+//        {
+//            "planName": "ao3-in-arrear-53",
+//                "phaseName": "ao3-in-arrear-53-evergreen",
+//                "phaseType": "EVERGREEN",
+//                "fixedPrice": null,
+//                "recurringPrice": 1.75,
+//                "usagePrices": [
+//            {
+//                "usageName": "ao3-usage",
+//                    "usageType": "CONSUMABLE",
+//                    "billingMode": "IN_ARREAR",
+//                    "tierBlockPolicy": "ALL_TIERS",
+//                    "tierPrices": [
+//                {
+//                    "blockPrices": [
+//                    {
+//                        "unitName": "bullets",
+//                            "size": 1,
+//                            "price": 1.35,
+//                            "max": 1000
+//                    }
+//              ]
+//                }
+//          ]
+//            }
+//      ]
+//        }
+//  ]
         osgiKillbillAPI.getSecurityApi().logout();
 
     }
